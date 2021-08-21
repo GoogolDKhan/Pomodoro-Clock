9330e201654c9c20d999393ee20d88339dd2e760
Sarfaraz <khansarfaraz2141@gmail.com>
2021-08-21 09:21:24 +0530

    include accurate interval js file

---
+++ js/accurate_interval.js
@@ -0,0 +1,21 @@
+// http://stackoverflow.com/questions/8173580/setinterval-timing-slowly-drifts-away-from-staying-accurate
+
+(function () {
+    window.accurateInterval = function (fn, time) {
+        var cancel, nextAt, timeout, wrapper;
+        nextAt = new Date().getTime() + time;
+        timeout = null;
+        wrapper = function () {
+            nextAt += time;
+            timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+            return fn();
+        };
+        cancel = function () {
+            return clearTimeout(timeout);
+        };
+        timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+        return {
+            cancel: cancel
+        };
+    };
+}).call(this);
